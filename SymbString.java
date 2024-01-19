65b9aa37a91a84716e05921214fefd292c7306cf
ShakirzyanovSH <81611249+ShakirzyanovSH@users.noreply.github.com>
2024-01-19 20:20:07 +0700

    Update SymbString.java

--- SymbString.java
+++ SymbString.java
@@ -1,1 +1,26 @@
+public class SymbString {
+	protected String id;
+  protected String content;
+
+    public SymbString(String id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Содержимое: " + content;
+    }
+
+    public static SymbString add(SymbString s1, SymbString s2) {
+        return new SymbString(s1.id + "+" + s2.id, s1.content + s2.content);
+    }
+}
 
